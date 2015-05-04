9a394bb28cfde3c97d0fca475e189048dd8da379
jobinwiztelsys <jobin@wiztelsys.com>
2015-05-04 14:36:06 +0530

    added the documentation for home screen. also added home_page.xml

--- app/src/main/java/complaintloggger/wiztelapp/com/complaint_logger/Home.java
+++ app/src/main/java/complaintloggger/wiztelapp/com/complaint_logger/Home.java
@@ -1,7 +1,28 @@
 package complaintloggger.wiztelapp.com.complaint_logger;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+
 /**
  * Created by Raju on 04-05-2015.
  */
-public class Home {
+public class Home extends ActionBarActivity{
+    /*The home class is loaded on successful user verification at the login process. the Home class
+     displays options to
+      1. choose an organization by fetching from the database
+      2. write down the complaint heading
+      3. write a complaint about a service or product in the organization
+      4. attach a photo as an evidence to the complaint by either
+           a. clicking a photo or
+           b. attaching photo from the external storage
+      5. submit button*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+
+
 }
