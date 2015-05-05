c97fe5a9331f1d06ef36b731fd03bf4afb94e0db
jobinwiztelsys <jobin@wiztelsys.com>
2015-05-05 18:01:33 +0530

    added edittexxt and string variables

--- app/src/main/java/complaintloggger/wiztelapp/com/complaint_logger/Home.java
+++ app/src/main/java/complaintloggger/wiztelapp/com/complaint_logger/Home.java
@@ -3,6 +3,7 @@ package complaintloggger.wiztelapp.com.complaint_logger;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.widget.EditText;
 
 /**
  * Created by Raju on 04-05-2015.
@@ -18,10 +19,15 @@ public class Home extends ActionBarActivity{
            b. attaching photo from the external storage
       5. submit button*/
 
+    EditText home_complaintHeadET,home_complaintET;  // edit text that receive complaint subject and complaint
+    String ComplaintHeadString, ComplaintString; //string that store complaint subject and complaint
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+        home_complaintHeadET=(EditText)findViewById(R.id.home_complaintHeadET);
+        home_complaintET=(EditText)findViewById(R.id.home_complaintET);
+
     }
 
 
