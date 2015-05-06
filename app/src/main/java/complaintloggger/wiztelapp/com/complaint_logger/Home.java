a526e012505e337d5957a0db3133aa54178e5d95
jobinwiztelsys <jobin@wiztelsys.com>
2015-05-06 18:10:40 +0530

    added design to the home page, fixed error in the splash screen for connectivity through wifi and mobile network.

--- app/src/main/java/complaintloggger/wiztelapp/com/complaint_logger/Home.java
+++ app/src/main/java/complaintloggger/wiztelapp/com/complaint_logger/Home.java
@@ -1,9 +1,15 @@
 package complaintloggger.wiztelapp.com.complaint_logger;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * Created by Raju on 04-05-2015.
@@ -20,13 +26,40 @@ public class Home extends ActionBarActivity{
       5. submit button*/
 
     EditText home_complaintHeadET,home_complaintET;  // edit text that receive complaint subject and complaint
-    String ComplaintHeadString, ComplaintString; //string that store complaint subject and complaint
+    String ComplaintHeadString, ComplaintString,OrganizationString; //string that store complaint subject  complaint, and organization name that is selected from spinner
+
+    private String[] organization_list = {
+            "Abundance",
+            "Anxiety",
+            "Bruxism",
+            "Discipline",
+            "Drug Addiction"
+            /*this list has to be replaced by the downloaded list of organizations*/
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
         home_complaintHeadET=(EditText)findViewById(R.id.home_complaintHeadET);
         home_complaintET=(EditText)findViewById(R.id.home_complaintET);
+
+
+        Spinner organizationListSpinner = (Spinner) findViewById(R.id.organizationListSpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.spinner_item, organization_list);
+        organizationListSpinner.setAdapter(adapter);
+        organizationListSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                OrganizationString=organization_list[position].toString();
+                Toast.makeText(getApplicationContext(),OrganizationString,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 
