2ba7fb0dadcfbb6514326d18f8179382db579b29
jobinwiztelsys <jobin@wiztelsys.com>
2015-05-12 17:17:09 +0530

    included the organization spinner loading  code

--- app/src/main/java/complaintloggger/wiztelapp/com/complaint_logger/Home.java
+++ app/src/main/java/complaintloggger/wiztelapp/com/complaint_logger/Home.java
@@ -1,8 +1,12 @@
 package complaintloggger.wiztelapp.com.complaint_logger;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,10 +15,23 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 /**
  * Created by Raju on 04-05-2015.
  */
 public class Home extends ActionBarActivity{
+
+    Servicehandler servicehandler = new Servicehandler();
+    Spinner organizationListSpinner;
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
+    String selctedcountryname;
+    static String url = "http://10.0.0.127/complaintlogger/fetchorglist.php";
     /*The home class is loaded on successful user verification at the login process. the Home class
      displays options to
       1. choose an organization by fetching from the database
@@ -24,18 +41,19 @@ public class Home extends ActionBarActivity{
            a. clicking a photo or
            b. attaching photo from the external storage
       5. submit button*/
+    ArrayList<String> organization_list = new ArrayList<String>(); //**** list to populate the spinner****//
+    Fetchorganization fetchorg= new Fetchorganization();
+
+    public  Home()
+   {
+
+   }
 
     EditText home_complaintHeadET,home_complaintET;  // edit text that receive complaint subject and complaint
     String ComplaintHeadString, ComplaintString,OrganizationString; //string that store complaint subject  complaint, and organization name that is selected from spinner
 
-    private String[] organization_list = {
-            "Abundance",
-            "Anxiety",
-            "Bruxism",
-            "Discipline",
-            "Drug Addiction"
-            /*this list has to be replaced by the downloaded list of organizations*/
-    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,24 +62,85 @@ public class Home extends ActionBarActivity{
 
         home_complaintHeadET=(EditText)findViewById(R.id.home_complaintHeadET);
         home_complaintET=(EditText)findViewById(R.id.home_complaintET);
+        sp = getSharedPreferences("isonetime", Context.MODE_PRIVATE);
+        editor = sp.edit();
+        selctedcountryname = sp.getString("selectedcountryname", "default");
 
+        organizationListSpinner = (Spinner) findViewById(R.id.organizationListSpinner);
 
-        Spinner organizationListSpinner = (Spinner) findViewById(R.id.organizationListSpinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.spinner_item, organization_list);
-        organizationListSpinner.setAdapter(adapter);
-        organizationListSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                OrganizationString=organization_list[position].toString();
-                Toast.makeText(getApplicationContext(),OrganizationString,Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
+        fetchorg.execute(selctedcountryname);
 
     }
+
+
+
+
+
+
+    //////////////////////////////////////////////////////////////////
+
+    public class Fetchorganization extends AsyncTask<String, Void, Void> {
+
+        Fetchorganization() {
+
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            Log.d("jobin", "selected country in the fetch organization class is " + strings[0]);
+            String result = servicehandler.makeServiceCall(url, strings[0]);
+            Log.d("jobin", "result given from the makeservicecall is: " + result);
+
+            try {
+                JSONArray jsonArray = null;
+                JSONObject j = null;
+                JSONObject jsonObject = new JSONObject(result);
+                jsonArray = jsonObject.getJSONArray("organization");
+
+                for (Integer i = 0; i < jsonArray.length(); i++) {
+                    j = jsonArray.getJSONObject(i);
+                    String list = j.getString("organization_name");
+                    Log.d("jobin", "list is " + list);
+                    organization_list.add(list);
+                    Log.d("jobin", "" + j.getString("organization_name"));
+
+
+                }
+                //  Intent k = new Intent(Splash_screen.this, Login.class);
+                //   startActivity(k);
+
+
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.spinner_item,organization_list);
+            organizationListSpinner.setAdapter(adapter);
+            organizationListSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    OrganizationString=organization_list.get(position).toString();
+                    Toast.makeText(getApplicationContext(),OrganizationString,Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        }
+    }
+
+
+///////////////////////////////////////////////////////////////////////////////////////
 
 
 
